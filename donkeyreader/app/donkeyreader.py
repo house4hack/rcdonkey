1c537dbfb7b33b46997d97a5ff388e80806a6cbe
skullkey <schalk.heunis@gmail.com>
2017-06-15 09:59:35 +0000

    removed opencv depency and using donkey camera

--- donkeyreader/app/donkeyreader.py
+++ donkeyreader/app/donkeyreader.py
@@ -2,50 +2,29 @@ import glob
 import serial
 import picamera
 import time
-import modules.camera as cam
-import cv2
 import struct
 import traceback
 import json
 import modules.util as util
 import os.path
 
+import donkey
+import donkey.utils
+import donkey.sensors
+
 conf = json.load(open("config.json"))
 
-if conf['load_pilot']:
-    import donkey
-    from donkey.pilots import KerasCategorical
-
-camera = cam.Camera(True,width=160,height=120)
+camera = donkey.sensors.PiVideoStream()
+camera.start()
 
 l = glob.glob("/dev/ttyS0")
 port = l[0]
 
-def create_img_filepath(directory, frame_count, angle, throttle, milliseconds):
-    filepath = str("%s/" % directory +
-                "frame_" + str(frame_count).zfill(5) +
-                "_ttl_" + str(throttle) +
-                "_agl_" + str(angle) +
-                "_mil_" + str(milliseconds) +
-                '.jpg')
-    return filepath
-
-
-def make_recording_folder(parent_folder):
-    l = glob.glob("%s/record_*" % parent_folder)
-    l.sort()
-    if len(l)>0:
-        last_record = l[-1]
-        parts = last_record.split("_")
-        number = int(parts[1])+1
-    else:
-        number = 1
-    return("%s/record_%05d/" % (parent_folder, number))
-
 is_recording= False
 is_deciding=False
-last_model=""
+last_model_name=""
 last_model_time=""
+pilot = None
 frame_no=0
 while True:
     with serial.Serial(port, 115200, timeout=1) as ser:
@@ -53,62 +32,51 @@ while True:
             ser.readline()
         line = ser.readline()   # read a '\n' terminated line
         try:
-            line = line.decode('utf-8')
-            parts = line.strip().split(",")
-            angle,throttle = util.convertFromPWM(parts[1],parts[0],conf)
-            dorecord = parts[3]=="1"
-            dodecide = parts[2]!="0"
+            angle,throttle,dorecord,dodecide = util.decode_line(line,conf)
             print("From Arduino:",throttle,angle,frame_no, dorecord,dodecide)
 
+            # Do we record? 
             if dorecord:
+                # mount and create new folder to store images
                 if not is_recording:
-                    if not is_deciding:
-                        util.mount()
-                    recording_folder = make_recording_folder(conf["save_folder"])
-                    if not os.path.exists(recording_folder):
-                        os.makedirs(recording_folder)
+                    util.mount()
+                    recording_folder = util.make_recording_folder(conf["save_folder"])
                     is_recording = True
-                frame = camera.grabFrame()
-                filename = create_img_filepath(recording_folder,frame_no,angle, throttle, 0.0)
+
+                # get image from camera and save to disk
+                frame = camera.capture_arr()
+                filename = util.create_img_filepath(recording_folder,frame_no,angle, throttle, 0.0)
                 print(filename)
-                cv2.imwrite(filename, frame)
+                img = donkey.utils.arr_to_img(frame)
+                img.save(filename)
                 frame_no+=1
             else:
+                # unmount when we are done
                 if is_recording:
                     is_recording = False                    
-                    if not is_deciding:
-                        util.umount()
+                    util.umount()
 
+            # are we deciding?
             if dodecide:
+                # load the pilot only if it has changed based on name and change date
                 if not is_deciding:
-                    if not is_recording:
-                        util.mount()
-                    # check if model on disk has changed
-                    l = glob.glob("%s/*.hdf5" % conf["model_folder"])
-                    if len(l)==0:
-                        print("Could not find model file!")
-                        util.umount()
-                    elif len(l)>0:
-                        model_file = l[0]
-                        model_time = time.strftime('%m/%d/%Y', time.gmtime(os.path.getmtime(model_file)))
-                        if not last_model==model_file and not last_model_time == model_time:
-                            pilot = KerasCategorical(model_file)
-                            pilot.load()
-                            last_model = model_file
-                            last_model_time = model_time
+                    util.mount()
+                    last_model_name, last_model_time, pilot = util.check_and_load_pilot(conf["model_folder"], last_model_name, last_model_time, pilot)
+                    if pilot != None:
                         is_deciding = True
+                    if not is_recording:
+                        util.umount()
 
                 if is_deciding:
-                    frame = camera.grabFrame()
+                    frame = camera.capture_arr()
                     angle,throttle = pilot.decide(frame)
                     angle_pwm,throttle_pwm= util.convertToPWM(angle, throttle,conf) 
                     print("To Arduino: %.2f: angle=%.2f throttle=%.2f angle_pwm=%d throttle_pwm=%d" % (time.time(), angle,throttle, angle_pwm, throttle_pwm))
                     util.sendToArduino(ser,angle_pwm,throttle_pwm)
             else:
+                # finished deciding
                 if is_deciding:
                     is_deciding = False
-                    if not is_recording:
-                        util.umount()
 
                 time.sleep(0.1)
         except Exception as e:
--- donkeyreader/app/modules/util.py
+++ donkeyreader/app/modules/util.py
@@ -1,6 +1,10 @@
 import struct
 import subprocess
 import os.path
+import glob
+import time
+from donkey.pilots import KerasCategorical
+
 
 def convertToPWM(angle,throttle,conf):
     angle = max(-0.5, angle)
@@ -50,3 +54,52 @@ def umount():
         print(proc)
     else:
         print("Not mounted, skipping")
+
+def create_img_filepath(directory, frame_count, angle, throttle, milliseconds):
+    filepath = str("%s/" % directory +
+                "frame_" + str(frame_count).zfill(5) +
+                "_ttl_" + str(throttle) +
+                "_agl_" + str(angle) +
+                "_mil_" + str(milliseconds) +
+               '.jpg')
+    return filepath
+
+
+def make_recording_folder(parent_folder):
+    l = glob.glob("%s/record_*" % parent_folder)
+    l.sort()
+    if len(l)>0:
+        last_record = l[-1]
+        parts = last_record.split("_")
+        number = int(parts[1])+1
+    else:
+        number = 1
+    recording_folder = "%s/record_%05d/" % (parent_folder, number)
+    if not os.path.exists(recording_folder):
+        os.makedirs(recording_folder)
+    return(recording_folder)
+
+def decode_line(line,conf):
+    line = line.decode('utf-8')
+    parts = line.strip().split(",")
+    angle,throttle = convertFromPWM(parts[1],parts[0],conf)
+    dorecord = parts[3]=="1"
+    dodecide = parts[2]!="0"
+    return(angle,throttle,dorecord,dodecide)
+
+def check_and_load_pilot(model_folder, last_model_name, last_model_time, pilot=None):
+    '''check if model has changed on disk and load pilot'''
+    l = glob.glob("%s/*.hdf5" % model_folder)
+    if len(l)==0:
+        print("Could not find model file!")
+        umount()
+    elif len(l)>0:
+        model_file = l[0]
+        model_time = time.strftime('%m/%d/%Y', time.gmtime(os.path.getmtime(model_file)))
+        if not last_model_name==model_file and not last_model_time == model_time:
+            pilot = KerasCategorical(model_file)
+            pilot.load()            
+        last_model_name = model_file
+        last_model_time = model_time
+    return(last_model_name, last_model_time, pilot)
+
